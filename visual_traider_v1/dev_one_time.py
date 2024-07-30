4558dff5a8ce6b3d7a88aebb3387c4f01bffdc8b
algoPVA <teacherPVA1012@gmail.com>
2024-07-30 23:41:14 +0300

    'some_change'

--- visual_traider_v1/dev_one_time.py
+++ visual_traider_v1/dev_one_time.py
@@ -1,15 +1,15 @@
 import pyautogui as pag
 import cv2
 from settings import configuration_traiders_v2
-from traider_bots.help_bots.WorkBot import WorkBot
+from traider_bots.help_bots.ResearchBot import ResearchBot
 param_bots = configuration_traiders_v2('config.txt')
-traider = WorkBot(*param_bots,name='MLTR')
+traider = ResearchBot(*param_bots,name='MLTR')
 online = False
 if online:
     pag.screenshot('Screen.png')
     img = cv2.imread('Screen.png')
 else:
-    img = cv2.imread('./test_data/24.07.24/images/SBER_1721808621.png')
+    img = cv2.imread('./learn_data/images/SOFL_1722342707.png')
 traider.run(img)
 cv2.imshow('work',img)
 cv2.moveWindow('work',-20,-20)
