c67a903adcfe25da4fb3a2e9129f7aa8b95d5ce5
Anubis1Good <vlad.polyakov.96@inbox.ru>
2024-09-08 13:25:24 +0300

    work on VT3

--- visual_traider_v1/test3.py
+++ visual_traider_v1/test3.py
@@ -1,92 +1,15 @@
-import cv2
-import numpy as np
-from random import randint
-from ml_funcs import draw_trends
-image = cv2.imread('Screenshot_6.png')
-image = cv2.imread('./09.07.24/images/SBER1720533379.8920171.png')
-image = image[0:540,0:480]
+import requests
+from urllib.parse import urlencode
 
-# constantes colors
-color1 = np.array([111,111,111])
-color2 = np.array([200,200,200])
-color_val1 = np.array([92,107,61])
-color_val2 = np.array([89,89,128])
+base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
+public_key = 'https://disk.yandex.ru/d/rK5z9ix2AoPpOg'  # Сюда вписываете вашу ссылку
 
-# get masks
-mask_val1 = cv2.inRange(image,color_val1,color_val1)
-mask_val2 = cv2.inRange(image,color_val2,color_val2)
-mask_val = cv2.add(mask_val1,mask_val2)
-mask1 = cv2.inRange(image,color1,color1)
-mask2 = cv2.inRange(image,color2,color2)
-mask = cv2.add(mask1,mask2)
-del mask2,mask1,mask_val1,mask_val2
-kernel = np.ones((2, 1), np.uint8) 
-mask = cv2.erode(mask,kernel)
+# Получаем загрузочную ссылку
+final_url = base_url + urlencode(dict(public_key=public_key))
+response = requests.get(final_url)
+download_url = response.json()['href']
 
-# get_volume statistic points
-cords_val = np.argwhere(mask_val == 255)
-max_val = np.min(cords_val[:,:1])
-mean_val = int(np.mean(cords_val,axis=0)[0])
-corners = cv2.goodFeaturesToTrack(mask_val,1000,0.01,1)
-corners = np.int32(corners) 
-
-for i in corners: 
-    x, y = i.ravel() 
-    cv2.circle(mask_val, (x, y), 1, (120,0,0), -1) 
-
-# get chart points
-cords_charts = np.argwhere(mask == 255)
-mean_charts = np.mean(cords_charts,axis=1)
-corners = cv2.goodFeaturesToTrack(mask,1000,0.01,1)
-corners = np.intp(corners) 
-shape = corners.shape
-corners = corners.reshape((shape[0],shape[2]))
-corners = corners[corners[:, 0].argsort()]
-# print(corners)
-for i in range(1,len(corners)-1): 
-    x1, y1 = corners[i].ravel()
-    x0, y0 = corners[i-1].ravel()
-    x2, y2 = corners[i+1].ravel()
-    if  y0 >= y1 <= y2 or y0 <= y1 >= y2:
-    # if  x0 >= x1 <= x2 or x0 <= x1 >= x2:
-        cv2.circle(image, (x1, y1), 2, (20,200,0), -1) 
-    cv2.circle(mask, (x1, y1), 1, (120,0,0), -1) 
-last_i = 0
-len_corners = len(corners)
-step = len_corners//3
-corners = corners[corners[:, 1].argsort()]
-# print(len_corners)
-for i in range(step-1,len_corners,step):
-    # print(i)
-    if len_corners - i < step:
-        distict = corners[last_i:len_corners-1]
-    else:
-        distict = corners[last_i:i]
-    last_i = i
-
-    y_max = np.max(distict[:,1:])
-    x_max = np.max(distict[:,:1])
-    y_min = np.min(distict[:,1:])
-    x_min = np.min(distict[:,:1])
-    color = (randint(0,255),randint(0,255),randint(0,255))
-    cv2.rectangle(image,(x_min,y_min),(x_max,y_max),color,2)
-    # cv2.circle(image,(y_max,x_max),3,(0,100,200),-1)
-# print(cords_charts)
-# print(mean_charts)
-x = corners[:,0]
-y = corners[:,1]
-# x = cords_charts[:,0]
-# y = cords_charts[:,1]
-# draw in image
-cv2.line(image,(0,mean_val),(400,mean_val),(250,200,10),2)
-draw_trends(x,y,image)
-# cv2.line(image,(0,max_val),(400,max_val),(200,200,9),2)
-
-# experiment
-# mask = cv2.blur(mask,(100,100))
-# show images
-cv2.imshow('chart',image)
-cv2.imshow('mask',mask)
-cv2.imshow('mask_val',mask_val)
-cv2.imwrite('mask_val.png',mask_val)
-cv2.waitKey(0)
+# Загружаем файл и сохраняем его
+download_response = requests.get(download_url)
+with open('downloaded_file.txt', 'wb') as f:   # Здесь укажите нужный путь к файлу
+    f.write(download_response.content)
---
+++ visual_traider_v1/traider_bots/VisualTraider_v3.py
@@ -0,0 +1,369 @@
+import traceback
+import os
+import time
+import cv2
+import numpy as np
+import numpy.typing as npt
+import pyautogui as pag
+import pydirectinput as pdi
+from scipy import stats
+from utils.config import ColorsBtnBGR
+from utils.chart_utils.dtype import HalfBar
+from utils.config import TemplateCandle
+from utils.test_utils.test_traide import test_open,test_close
+
+class VisualTraider_v3():
+    def __init__(
+            self,
+            cluster:tuple,
+            dealfeed:tuple,
+            glass:tuple,
+            chart:tuple,
+            position:tuple,
+            name:str,
+            mode:int = 0) -> None:
+        self.cluster_region = cluster
+        self.dealfeed_region = dealfeed
+        self.glass_region = glass
+        self.chart_region = chart
+        self.position_region = position
+        self.name = name 
+        self.traider_name = 'VisualTraider_v3'
+        self.mode = mode
+        self.TA = None
+
+    def __repr__(self) -> str:
+        return f'{self.traider_name} - {self.name}'
+    
+    # work function
+    def _test(self, img):
+        action,keys = self.ТА(self,img)
+        res1_l,res2_l = 0,0
+        res1_s,res2_s = 0,0
+        if action == 'long':
+            res1_s = self._test_send_close(img,'short')
+            res2_l = self._test_send_open(img,'long') 
+        if action == 'close_long':
+            res1_l = self._test_send_close(img,'long')
+        if action == 'short':
+            res1_l = self._test_send_close(img,'long')
+            res2_s = self._test_send_open(img,'short')
+        if action == 'close_short':
+            res1_s = self._test_send_close(img,'short')
+        if res1_s == 1 and res2_l == 0:
+            self.have_pos_s = False
+            self.free_stop_s = True
+        if res1_l == 1 and res2_s == 0:
+            self.have_pos_l = False
+            self.free_stop_l = True
+        if res2_l == 1:
+            self.have_pos_l = True
+            self.have_pos_s = False
+            self.free_stop_l = False
+            self.free_stop_s = True
+        if res2_s == 1:
+            self.have_pos_s = True
+            self.have_pos_l = False
+            self.free_stop_s = False
+            self.free_stop_l = True
+        self.write_logs(keys,action)
+
+    def _traide(self, img):
+        pos = self._check_position(img)
+        if pos == 0:
+            self.have_pos_l = False
+            self.have_pos_s = False
+            self.free_stop_l = True
+            self.free_stop_s = True
+        if pos == 1:
+            self.have_pos_l = True
+            self.have_pos_s = False
+            self.free_stop_l = False
+            self.free_stop_s = True
+        if pos == -1:
+            self.have_pos_l = False
+            self.have_pos_s = True
+            self.free_stop_l = True
+            self.free_stop_s = False
+        action,keys = self.ТА(self,img)
+        if action:
+            if action == 'long':
+                if pos == -1:
+                    self.close_short = True
+                    self._reverse_pos(img,'long')
+                if pos == 0:
+                    self._send_open('long')
+            if action == 'close_long':
+                if pos == 1:
+                    self.close_long = True
+                    self._send_close(img,'long')
+            if action == 'short':
+                if pos == 1:
+                    self.close_long = True
+                    self._reverse_pos(img,'short')
+                if pos == 0:
+                    self._send_open('short')
+            if action == 'close_short':
+                if pos == -1:
+                    self.close_short = True
+                    self._send_close(img,'short')
+        elif self.close_long:
+            if pos == 1:
+                self._send_close(img,'long')
+            else:
+                self.close_long = False
+        elif self.close_short:
+            if pos == -1:
+                self._send_close(img,'short')
+            else:
+                self.close_long = False
+        else:
+            self._reset_req()
+
+    
+    def run(self,img):
+        copy_img = img.copy()
+        try:
+            if self.mode == 0:
+                self._test(copy_img)
+            elif self.mode == 1:
+                self._traide(copy_img)
+            elif self.mode == 2:
+                self._traide(copy_img)
+                self._test(copy_img)
+        except Exception as err:
+            traceback.print_exc()
+        return copy_img
+
+    # terminal_function
+    def _check_position(self,img) -> int:
+        x,y = self._color_search(img,ColorsBtnBGR.best_bid,self.position_region)
+        if x >= 0:
+            return 1
+        x,y = self._color_search(img,ColorsBtnBGR.best_ask,self.position_region)
+        if x >= 0:
+            return -1
+        return 0
+    
+    def _check_req(self,img) ->tuple | bool:
+        x,y = self._color_search(img,ColorsBtnBGR.color_x_shadow,self.glass_region)
+        if x>0:
+            return x,y
+        else:
+            x,y = self._color_search(img,ColorsBtnBGR.color_x,self.glass_region)
+            if x > 0:
+                return x,y
+            else:
+                x,y = self._color_search(img,ColorsBtnBGR.color_x_bb,self.glass_region)
+                return x,y
+            
+    
+    # trade_function
+    def _send_open(self,direction):
+        pag.moveTo(self.glass_region[0]+11,self.glass_region[1]+11)
+        pdi.press('f')
+        if direction == 'long':
+            button = 'a'
+        elif direction == 'short':
+            button = 's'
+        else:
+            button = 'f'
+        pdi.press(button)
+
+    def _send_close(self,img,direction):
+        if direction == 'long':
+            x,y = self._color_search(img, ColorsBtnBGR.best_ask,self.glass_region,reverse=True)
+            if x < 0:
+                x,y = self._color_search(img, ColorsBtnBGR.ask,self.glass_region,reverse=True)
+            x -= 50
+            y -= 5
+            button = 'right'
+        elif direction == 'short':
+            x,y = self._color_search(img, ColorsBtnBGR.best_bid,self.glass_region,reverse=False)
+            if x < 0:
+                x,y = self._color_search(img, ColorsBtnBGR.bid,self.glass_region,reverse=False)
+            x += 10
+            y += 5
+            button = 'left'
+        else:
+            return None
+        pag.moveTo(x,y)
+        pdi.press('f')
+        pdi.keyDown('altleft')
+        pag.click(x, y,button=button)
+        pdi.keyUp('altleft')
+    
+    def _reverse_pos(self,img,direction):
+        if direction == 'long':
+            button = 'a'
+            x,y = self._color_search(img, ColorsBtnBGR.best_bid,self.glass_region,reverse=False)
+            if x < 0:
+                x,y = self._color_search(img, ColorsBtnBGR.bid,self.glass_region,reverse=False)
+            x += 10
+            y += 5
+            button_m = 'left'
+        elif direction == 'short':
+            button = 's'
+            x,y = self._color_search(img, ColorsBtnBGR.best_ask,self.glass_region,reverse=True)
+            if x < 0:
+                x,y = self._color_search(img, ColorsBtnBGR.ask,self.glass_region,reverse=True)
+            x -= 50
+            y -= 5
+            button_m = 'right'
+        else:
+            button = 'f'
+            return None
+        pag.moveTo(x,y)
+        pdi.press('f')
+        pdi.press(button)
+        pdi.keyDown('altleft')
+        pag.click(x, y,button=button_m)
+        pdi.keyUp('altleft')
+
+    def _reset_req(self):
+        pag.moveTo(self.glass_region[0]+11,self.glass_region[1]+11)
+        pdi.press('f')
+    # test trade_function
+
+    def _test_send_open(self,img,direction,draw=lambda img:img):
+        return test_open(img,self.name,direction,self.traider_name,draw)
+
+    def _test_send_close(self,img,direction,draw=lambda img:img):
+        return test_close(img,self.name,direction,self.traider_name,draw)
+
+    def _draw(self,img,keys,region):
+        pass
+
+    # chart_function
+    def _get_chart(self,img,region):
+        chart = img[
+        region[1]:region[3],
+        region[0]:region[2]]
+        return chart
+    
+    def _get_current_price(self,chart):
+        x,y = self._color_search(chart,ColorsBtnBGR.cur_price_1,reverse=True)
+        if y > 0:
+            x,y2 = self._color_search(chart,ColorsBtnBGR.cur_price_1,reverse=False)
+            return (x,(y+y2)//2)
+        x,y = self._color_search(chart,ColorsBtnBGR.cur_price_2,reverse=True)
+        if y > 0:
+            x,y2 = self._color_search(chart,ColorsBtnBGR.cur_price_2,reverse=False)
+            return (x,(y+y2)//2)
+        return None,None    
+    
+    def _get_mask(self,chart:npt.ArrayLike,color) -> npt.ArrayLike:
+        mask = cv2.inRange(chart,color,color)
+        return mask
+    
+    def _get_candle_mask(self,chart:npt.ArrayLike) -> npt.ArrayLike:
+        mask1 = self._get_mask(chart,ColorsBtnBGR.candle_color_1)
+        mask2 = self._get_mask(chart,ColorsBtnBGR.candle_color_2)
+        mask = cv2.add(mask1,mask2)
+        kernel = np.ones((2, 1), np.uint8) 
+        mask = cv2.erode(mask,kernel)
+        return mask
+
+    def _get_volume_mask(self,chart:npt.ArrayLike) -> npt.ArrayLike:
+        mask1 = self._get_mask(chart,ColorsBtnBGR.volume_color_1)
+        mask2 = self._get_mask(chart,ColorsBtnBGR.volume_color_2)
+        mask = cv2.add(mask1,mask2)
+        return mask
+    
+    def _get_cords_on_mask(self,mask:npt.ArrayLike) -> npt.NDArray:
+        cords = np.argwhere(mask == 255)
+        return cords
+    
+    def _get_half_bars(
+            self,
+            candle_mask: npt.ArrayLike,
+            candle_cords: npt.NDArray, 
+            volume_cords: npt.NDArray
+            ) -> list[HalfBar]: 
+        res_top = cv2.matchTemplate(candle_mask,TemplateCandle.candle_top,cv2.TM_CCOEFF_NORMED)
+        res_top = np.argwhere(res_top >= 0.9)
+        res_top = res_top[res_top[:, 1].argsort()]
+        half_bars:list[HalfBar] = []
+        for i in range(res_top.shape[0]):
+            res_top[i] = (res_top[i][0],res_top[i][1]+1)
+            point_b = candle_cords[np.where(candle_cords[:,1] == res_top[i][1])]
+            point_v = volume_cords[np.where(volume_cords[:,1] == res_top[i][1])]
+            y_b = point_b[:,0].max()
+            y_v = point_v[:,0].min()
+            half_bars.append(HalfBar(res_top[i][1],res_top[i][0],y_b,y_v))
+        return np.array(half_bars)
+    
+    def _get_mean(self,cords:npt.NDArray):
+        mean_val = (10,int(np.mean(cords,axis=0)[0]))
+        return mean_val
+    
+    def _get_limit(self,cords:npt.NDArray):
+        max_val = (10,np.min(cords[:,:1]))
+        min_val = (10,np.max(cords[:,:1]))
+        return max_val,min_val
+    
+    def _get_xy(self,points:npt.NDArray) -> npt.NDArray:
+        x = points[:,0]
+        y = points[:,1]
+        return x,y
+    
+    def _get_points(self,half_bars:list[HalfBar]) -> npt.NDArray:
+        points = []
+        for hb in half_bars:
+            points.append(hb.hpt)
+            points.append(hb.lpt)
+        return np.array(points)
+
+    def _get_trend_lines(self,x,y):
+        std_y = np.std(y)
+        slope,intercept = self._get_linear_regress(x,y)
+        middle_line = list(map(lambda x:self._get_points_linear_reg(x,slope,intercept,0), x))
+        top_line = list(map(lambda x:self._get_points_linear_reg(x,slope,intercept,-std_y), x))
+        bottom_line = list(map(lambda x:self._get_points_linear_reg(x,slope,intercept,std_y), x))
+        trend = np.column_stack([x, middle_line])
+        top_trend = np.column_stack([x, top_line])
+        bottom_trend = np.column_stack([x, bottom_line])
+        return trend,top_trend,bottom_trend,slope
+    
+        # help function
+    def _change_coords(self,point,region:tuple) -> tuple:
+        point = list(point)
+        point[0] += region[0]
+        point[1] += region[1]
+        return tuple(point)
+    
+    def _color_search(self,img:npt.ArrayLike,color:tuple[int],region:tuple[int]=(None,None,None,None),reverse:bool=False):
+        try:
+            result = np.argwhere(
+                (img[region[1]:region[3],region[0]:region[2],0] == color[0])& 
+                (img[region[1]:region[3],region[0]:region[2],1] == color[1])& 
+                (img[region[1]:region[3],region[0]:region[2],2] == color[2])
+            )
+            y = -1 if reverse else 0
+            if region[0]:
+                return result[y,1]+region[0], result[y,0]+region[1]
+            return result[y,1],result[y,0]
+
+        except Exception:
+            # traceback.print_exc()
+            return -1,-1
+    
+    # ml function
+    def _get_linear_regress(self,x,y):
+        slope, intercept, r, p, std_err = stats.linregress(x, y)
+        return round(slope,4),intercept
+
+    def _get_points_linear_reg(self,x,slope,intercept,offset=0):
+        return int(slope * x + intercept+offset)
+
+
+    # debug function
+    def write_logs(self,keys,action):
+        file_name = f'./logs/{self.traider_name}{self.name}.txt'
+        log = f'{self.name} - {time()} - {action}: state: {vars(keys)}\n'
+        if not os.path.exists(file_name):
+            with open(file_name, 'w') as f:
+                f.write(log)
+        else:
+            with open(file_name, 'a') as f:
+                f.write(log)
