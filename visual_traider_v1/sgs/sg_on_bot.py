1977ca60ad980b82ad72f5b542dce1791506e0b7
Anubis1Good <vlad.polyakov.96@inbox.ru>
2025-03-19 00:14:10 +0300

    update robots

--- visual_traider_v1/sgs/sg_on_bot.py
+++ visual_traider_v1/sgs/sg_on_bot.py
@@ -6,14 +6,15 @@ sleep_group = ['PIKK','MRKP','GAZP','BSPB','SBER']
     # 'PTA2_DDC_60' : [],
     # 'PTA2_DDC_80' : ['AFKS'],
     # 'LTA1_C':['SIBN','ALRS','APTK','FEES','ABIO'],
-    # 'PTA4_WDDC_100_10_20':[,'NLMK','MAGN','SOFL']
+    # 'PTA4_WDDC_100_10_20':[,'NLMK','SOFL']
 
 bot_on_ticker = {
-    'PTA4_WDDCrRL_11_11_30' : ['ROSN','MXI','MTLR','MOEX','CHMF'],
-    'PTA4_WDDCrRL_6_6_30' : ['GMKN','GZH5','SELG','CNY','SBRF','GAZR'],
-    'PTA4_WDVCrRL_11_11_30' : ['NVTK','RUAL','TATN','LKOH'],
-    'PTA8_WDOBBYFrRL_11_0p5_11_30' : ['VTBR',],
-    'PTA4_WDDCrRL_10_10_20' : ['MOEX'],
+    'PTA4_WDDCrRL_11_11_30' : ['MTLR','MOEX','CHMF'],
+    'PTA4_WDDCrRL_6_6_30' : ['VTBR','TATN','GMKN','CNY','RUAL','SBRF'],
+    'PTA4_WDVCrRL_11_11_30' : ['ROSN','LKOH'],
+    'PTA8_WDOBBYFrRL_11_0p5_11_30' : ['SELG','NVTK','MAGN'],
+    'PTA4_WDDCrRL_10_10_20' : ['MOEX','GAZR'],
+    'PTA4_WDDCrRL_4_4_30' : ['MXI',],
 }
 
 bot_on_ticker_5m = {
